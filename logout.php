c38eed184ce3982f2dc1820be19f4d0f97055917
ADMIN <morrison0899@gmail.com>
2017-04-03 21:59:01 +0100

    db connect Commit

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 03/04/2017
+ * Time: 21:53
+ */
+session_start();
+unset($_SESSION['name']);
+session_destroy();
+header("location:index.php");

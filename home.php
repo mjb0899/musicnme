3137519d6c8767a76d3e04db5af175d0d2943c2d
ADMIN <morrison0899@gmail.com>
2017-04-06 18:47:28 +0100

    db connect Commit

--- home.php
+++ home.php
@@ -51,7 +51,7 @@ $acc=$_SESSION['acctype'];
                     <li><a href="aboutus.php">About Us</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li <?php if(isset($sess)) { ?>style="display:none;"<?php } ?>><a href="#"><span class="glyphicon glyphicon-user" ></span> Sign Up</a></li>
+                    <li id="hiding" <?php if(isset($sess)) { ?>style="display:none;"<?php } ?>><a href="#"><span class="glyphicon glyphicon-user" ></span> Sign Up</a></li>
                     <li <?php if(isset($sess)) { ?>style="display:none;"<?php } ?>><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                     <li <?php if(isset($sess)&&($acc=='artist')){ ?>style="display:none;"<?php } ?> ><a href="userprofile.php"><span class="glyphicon glyphicon-headphones"></span> <?php echo $sess ?></a></li>
                     <li <?php if(isset($sess)&&($acc=='user')) { ?>style="display:none;"<?php } ?> ><a href="artistprofile.php"><span class="glyphicon glyphicon-headphones"></span> <?php echo $sess ?></a></li>

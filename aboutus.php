78407d495bbcb36e3e879e6197de32e50beaf9f8
ADMIN <morrison0899@gmail.com>
2017-04-06 04:13:20 +0100

    db connect Commit

--- aboutus.php
+++ aboutus.php
@@ -43,8 +43,9 @@ $acc=$_SESSION['acctype'];
                 <ul class="nav navbar-nav navbar-right">
                     <li <?php if(isset($sess)) { ?>style="display:none;"<?php } ?>><a href="#"><span class="glyphicon glyphicon-user" ></span> Sign Up</a></li>
                     <li <?php if(isset($sess)) { ?>style="display:none;"<?php } ?>><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                    <li id="art" <?php if(isset($sess)&&($acc=='artist')){ ?>style="display:inline;"<?php } ?> ><a href="artistprofile.php"><span class="glyphicon glyphicon-headphones"></span> <?php echo $sess ?></a></li>
-                    <li id="usr" <?php if(isset($sess)&&($acc=='user')) { ?>style="display:inline;"<?php } ?> ><a href="userprofile.php"><span class="glyphicon glyphicon-headphones"></span> <?php echo $sess ?></a></li>
+                    <li <?php if(!isset($sess)) { ?>style="display:none;"<?php } ?> ><span class="glyphicon glyphicon-headphones"></span></li>
+                    <li <?php if(isset($sess)&&($acc=='artist')){ ?>style="display:none;"<?php } ?> ><a href="userprofile.php"><span class="glyphicon glyphicon-headphones"></span> <?php echo $sess ?></a></li>
+                    <li <?php if(isset($sess)&&($acc=='user')) { ?>style="display:none;"<?php } ?> ><a href="artistprofile.php"><span class="glyphicon glyphicon-headphones"></span> <?php echo $sess ?></a></li>
                     <li <?php if(!isset($sess)) { ?>style="display:none;"<?php } ?> ><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
 
                 </ul>

0f451625be0c5612b604491469d9a78e12eef07e
ADMIN <morrison0899@gmail.com>
2017-04-09 20:49:47 +0100

    db connect Commit

--- home.php
+++ home.php
@@ -71,19 +71,64 @@ $artist=$_SESSION['artist'];
             <div class="two">
                 <div class="share">
                     <div class="share_align">
-                        <form action="upload.php" method="post" enctype="multipart/form-data">
-                            <div class="form-group">
-                                <label for="exampleInputFile">File input</label>
-                                <input type="file" id="exampleInputFile" name="file">
-                                <p class="help-block">Example block-level help text here.</p>
+                        <div class="container">
+                            <ul class="nav nav-pills">
+                                <li class="active"><a data-toggle="pill" href="#home">Create a Post</a></li>
+                                <li><a data-toggle="pill" href="#menu1">Share Music</a></li>
+                                <li><a data-toggle="pill" href="#menu2">Men</a></li>
+                                <li><a data-toggle="pill" href="#menu3">Menu 3</a></li>
+                            </ul>
+
+                            <div class="tab-content">
+                                <div id="home" class="tab-pane fade in active">
+                                    <div class="container">
+                                        <!-- Trigger the modal with a button -->
+                                        <input type="text" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" placeholder="What's on your mind?" id="col">
+
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="myModal" role="dialog">
+                                            <div class="modal-dialog modal-sm">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                        <h4 class="modal-title">What's on your mind?</h4>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <textarea class="form-control" rows="5" id="comment"></textarea>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-default" data-dismiss="modal" id="button_col">Post</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="menu1" class="tab-pane fade">
+                                    <form action="upload.php" method="post" enctype="multipar\t/form-data">
+                                        <div class="form-group">
+                                            <label for="exampleInputFile">File input</label>
+                                            <input type="file" id="exampleInputFile" name="file">
+                                            <p class="help-block">Example block-level help text here.</p>
+                                        </div>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox"> Check me out
+                                            </label>
+                                        </div>
+                                        <button type="submit" class="btn btn-default" id="submit_btn" name="submit">Post</button>
+                                    </form>
+                                </div>
+                                <div id="menu2" class="tab-pane fade">
+                                    <h3>Menu 2</h3>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                </div>
+                                <div id="menu3" class="tab-pane fade">
+                                    <h3>Menu 3</h3>
+                                    <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                                </div>
                             </div>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox"> Check me out
-                                </label>
-                            </div>
-                            <button type="submit" class="btn btn-default" id="submit_btn" name="submit" >Post</button>
-                        </form>
+                        </div>
                     </div>
                 </div>
                 <div class="feed">

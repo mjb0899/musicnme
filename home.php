aa1914b34ba6399fcdbb08dc20bc2d45337b1b34
ADMIN <morrison0899@gmail.com>
2017-04-11 13:14:48 +0100

    db connect Commit

--- home.php
+++ home.php
@@ -104,7 +104,7 @@ $artist=$_SESSION['artist'];
                                                 <input type="checkbox"> Check me out
                                             </label>
                                         </div>
-                                        <button type="submit" class="btn btn-default" id="submit_btn" name="submit">Post</button>
+                                        <button type="submit" class="btn btn-default" id="button_col" name="submit">Post</button>
                                     </form>
                                 </div>
                                 <div id="menu2" class="tab-pane fade">
@@ -128,7 +128,7 @@ $artist=$_SESSION['artist'];
                                                 <input type="checkbox"> Check me out
                                             </label>
                                         </div>
-                                        <button type="submit" class="btn btn-default" id="submit_btn" name="submit">Post</button>
+                                        <button type="submit" class="btn btn-default" id="#button_col" name="submit">Post</button>
                                     </form>
                                 </div>
                                 <div id="menu3" class="tab-pane fade">

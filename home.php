7a7cdf7ea2353f26e85dbf26f0b12af1ffe534c2
ADMIN <morrison0899@gmail.com>
2017-04-11 18:53:35 +0100

    db connect Commit

--- home.php
+++ home.php
@@ -188,8 +188,7 @@ $artist=$_SESSION['artist'];
                               
                               
                               <audio controls>
-                                        <source src=\"horse.ogg\" type=\"audio/ogg\">
-                                         <source src=\"horse.mp3\" type=\"audio/mpeg\">
+                                         <source src=\"music/horse.mp3\" type=\"audio/mpeg\">
                                 </audio>
                               
                               

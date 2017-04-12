a6f04624a75e58f605f335b4a16276b3257062e9
ADMIN <morrison0899@gmail.com>
2017-04-12 01:55:32 +0100

    db connect Commit

--- home.php
+++ home.php
@@ -184,7 +184,7 @@ $artist=$_SESSION['artist'];
                      <p>$status</p>
                      </div>
                      </div>
-                     <div style='height: 500px; width: 500px; background-color: #212121;border:2px solid black; margin: auto'>
+                     <div style='height: 400px; width: 500px; background-color: #212121;margin: auto'>
                       <img style='height: 100%; width: 100%; object-fit: contain' src=".$file_path.">      
                     </div>
                      

5490b6087048beaad051075958cac7a4ae4f4707
ADMIN <morrison0899@gmail.com>
2017-04-13 15:58:27 +0100

    db connect Commit

--- artistprofile.php
+++ artistprofile.php
@@ -130,6 +130,95 @@ $owner=$_GET['owner'];
             <div id="about" class="tabcontent">
                 <?php
                 include("dbConnect.php");
+                $sql_query = "Select * from post_info WHERE username='$owner' ORDER BY dateposted DESC ";
+                $result = $db -> query($sql_query);
+                while($row = $result -> fetch_array()) {
+                    $post_owner = $row['username'];
+                    $status = $row['description'];
+                    $file_name = $row['file_name'];
+                    $file_path = $row['path'];
+                    $file_title = $row['title'];
+                    $file_type = $row['file_type'];
+                    $profile_pic = $row['profile_image'];
+                    $post_owner_type=$row['utype'];
+
+                    // retrieve profile else default pic
+                    if($profile_pic!=null){
+
+                    }else{
+                        $profile_pic="images/default_profile.png";
+                    }
+
+                    $allowed_image = array('jpg','jpeg','png');
+                    $allowed_media = array('mp4','mp3');
+
+                    //uploads image and music
+                    if($file_title==="upload") {
+                        if(in_array($file_type,$allowed_image)) {
+                            echo "<div class=\"media\">
+                     <div class=\"media-left\">
+                     <img src=\"$profile_pic\" class=\"media-object\" style=\"width:60px\">
+                     </div>
+                     <div class=\"media-body\">
+                     <h4 class=\"media-heading\">$post_owner</h4>
+                     <p>$status</p>
+                     </div>
+                     </div>
+                     <div style='height: 300px; width: 400px; background-color: #212121;margin: auto'>
+                      <img style='height: 100%; width: 100%; object-fit: contain' src=".$file_path.">      
+                    </div>
+                     
+                     
+                     
+                     
+                     
+                     <hr>";
+
+
+
+
+
+
+
+                        }elseif(in_array($file_type,$allowed_media)){
+                            echo "<div class=\"media\">
+                     <div class=\"media-left\">
+                     <img src=\"$profile_pic\" class=\"media-object\" style=\"width:60px\">
+                     </div>
+                     <div class=\"media-body\">
+                     <h4 class=\"media-heading\">$post_owner</h4>
+                     <p>$status</p>
+                     </div>
+                     </div>
+                     <div style='border-left:2px solid #ff3333; padding: 1rem; margin-top: 1rem;margin-left: 1rem'>
+                     <audio controls style='width: 100%;'>
+                     <source src=\"$file_path\" type=\"audio/mpeg\">
+                     </audio>
+                     </div>
+                     <hr>";
+                        }
+                    }
+
+
+
+
+
+
+
+
+
+                }//end while
+
+
+
+
+
+                ?>
+
+            </div>
+            <div id="events" class="tabcontent">
+                <?php
+                include("dbConnect.php");
                 $sql_query = "Select ufname,ulname,uemail from users Where username='$sess'";
                 $result = $db -> query($sql_query);
                 while($row = $result -> fetch_array()){
@@ -139,12 +228,6 @@ $owner=$_GET['owner'];
                     echo "<h2>$firstname"." "."$lastname$owner</h2>";
                 }
                 ?>
-                <h3>Paris</h3>
-                <p>Paris is the capital of France.</p>
-            </div>
-            <div id="events" class="tabcontent">
-                <h3>Tokyo</h3>
-                <p>Tokyo is the capital of Japan.</p>
             </div>
             <div id="followme" class="tabcontent">
                 <h3>Tokyo</h3>

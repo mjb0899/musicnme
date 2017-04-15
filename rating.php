9e9f134268a15705a6b3fc501c61bb80a392bd43
ADMIN <morrison0899@gmail.com>
2017-04-16 00:56:55 +0100

    rating commit

--- rating.php
+++ rating.php
@@ -21,12 +21,32 @@ include ("dbConnect.php");
 $topic=$_POST['topic'];
 $topic_array=explode('_',$topic);
 $tid=$topic_array[0];
-
+$count=0;
+$totalRate=0;
 $sql_query = "Select uid from users WHERE username='$sess'";
 $result = $db -> query($sql_query);
 while($row = $result -> fetch_array()) {
     $uid = $row['uid'];
-}
+}   //get count values
+    $sql3="select rate from uid where tid='$tid'";
+    $result = $db -> query($sql3);
+    while($row = $result -> fetch_array()){
+        $totalRate=$totalRate+$row['rate'];
+        $count=$count+1;
+    }
+    $hits=($totalRate/$count);
+    //set count values
+    $sql4="UPDATE topic SET hits=$hits where tid='$tid'";
+
+    $result = $db -> query($sql4);
+    if(mysqli_query($db,$sql)){
+
+    }
+    else{
+    echo"Error:".$sql."<br>" . mysqli_error($db);
+    }
+
+
 
 
 
@@ -42,9 +62,10 @@ if (isset($_POST['rate']) && !empty($_POST['rate'])) {
         echo $row['rid'];
     } else {
 
-        $sql2 = "INSERT INTO tbl_rating (rate,tid,uid) VALUES ('$rate','$tid','$uid'); ";
+        $sql2 = "INSERT INTO tbl_rating (rate,tid,uid) VALUES ('$rate','$tid','$uid')";
         if (mysqli_query($db, $sql2)) {
             echo "0";
         }
+
     }
 }

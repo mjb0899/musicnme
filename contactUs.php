4c7c89f00fcf5717b81496a8b2e757bde7109836
ADMIN <morrison0899@gmail.com>
2017-04-16 19:15:44 +0100

    rating commit

--- contactUs.php
+++ contactUs.php
@@ -23,17 +23,21 @@ $acc=$_SESSION['acctype'];
     <!--FOOTER CSS-->
     <link rel="stylesheet" type="text/css" href="css/footer.css">
     <script>
-        $('#myForm').submit(function(e){
-            e.preventDefault();
-            $.ajax({
-                url:'saveFeedback.php',
-                type:'post',
-                data:$('#myForm').serialize(),
-                success:function(){
-                    alert("Our team will get back to you via email! Thank You!")
-                }
-            });
+        function chk() {
+        var subject=document.getElementById('subject').value;
+        var matter=document.getElementById('matter').value;
+        var dataString= 'name='+subject+'&matter='+matter;
+        $.ajax({
+            type:"post",
+            url:"saveFeedback.php",
+            data:dataString,
+            cache:false,
+            success.function(html){
+                $('#alert').html(html);
+            }
         });
+        return false;
+        }
     </script>
     <style>
 
@@ -88,7 +92,7 @@ $acc=$_SESSION['acctype'];
                     <textarea  class="form-control" id="matter" placeholder="Give a short Description" name="matter" required></textarea>
                 </div>
                 <button type="submit" class="btn btn-default">Submit</button>
-                <div class="alert success" >Thank you for getting in touch! Our team will get back to you via email!</div>
+                <div id="alert" >Thank you for getting in touch! Our team will get back to you via email!</div>
             </form>
         </div>
 

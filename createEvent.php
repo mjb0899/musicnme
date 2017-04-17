43d76cd90eb9e1c7df4405a3e34d54ce8397c76a
ADMIN <morrison0899@gmail.com>
2017-04-17 21:38:00 +0100

    rating commit

--- createEvent.php
+++ createEvent.php
@@ -112,7 +112,6 @@ $acc=$_SESSION['acctype'];
 </header>
 <!--MAIN CODE-->
 <main>
-        <div class="img_contain">
             <div class="wrapper">
                     <div class="wrapper_head">
                         <h2>Start Event:</h2>
@@ -149,7 +148,6 @@ $acc=$_SESSION['acctype'];
                     </form>
 
 
-            </div>
 
         </div>
 

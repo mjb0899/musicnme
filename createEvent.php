7dfe082bd41b22b8eb9d69f8be11fc37b458c724
ADMIN <morrison0899@gmail.com>
2017-04-17 20:15:29 +0100

    rating commit

--- createEvent.php
+++ createEvent.php
@@ -24,7 +24,7 @@ $acc=$_SESSION['acctype'];
     <link rel="stylesheet" type="text/css" href="css/footer.css">
 
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="css/aboutus.css">
+    <link rel="stylesheet" href="css/event.css">
 
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -116,7 +116,7 @@ $acc=$_SESSION['acctype'];
 
     <div class="wrapper">
 
-        <div class="wrapper_event">
+
             <div class="container">
                 <div>
                     <h2>Get In Touch:</h2>
@@ -158,8 +158,6 @@ $acc=$_SESSION['acctype'];
 
 
 
-
-    </div>
 
 
 

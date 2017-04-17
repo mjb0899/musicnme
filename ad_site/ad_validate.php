4f3b167a69c88196d1f5d443395ad60e2acf13f0
ADMIN <morrison0899@gmail.com>
2017-04-17 17:01:26 +0100

    rating commit

--- ad_site/ad_validate.php
+++ ad_site/ad_validate.php
@@ -5,13 +5,13 @@
  * Date: 17/04/2017
  * Time: 16:13
  */
-
+if(isset($_POST))
 $username=$_POST["username"];
 $password=$_POST["password"];
 
 if(($username=='lucifer')&&($password=='xxxx')){
     session_start();
     $_SESSION["ad_access"]=1;
-    header("location:ad_home.html");
+    header("location:ad_home.php");
     exit();
 }

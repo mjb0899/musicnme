d078a8cb0e4d1b8ff26227be5629d23859e07cae
ADMIN <morrison0899@gmail.com>
2017-04-18 19:07:06 +0100

    rating commit

--- home.php
+++ home.php
@@ -97,6 +97,16 @@ $artist=$_SESSION['artist'];
                     <a href="userprofile.php"><h2><?php echo $sess  ?></h2></a>
                 </div>
                 <div class="profile_data">
+                    <div class="container">
+                        <h2>Simple Collapsible</h2>
+                        <p>Click on the button to toggle between showing and hiding content.</p>
+                        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Simple collapsible</button>
+                        <div id="demo" class="collapse">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
 
                 </div>
             </div>

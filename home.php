22e5d5122ec9f1f66e7003a8dad4c98fc9f7e877
ADMIN <morrison0899@gmail.com>
2017-04-18 19:22:29 +0100

    rating commit

--- home.php
+++ home.php
@@ -101,9 +101,13 @@ $artist=$_SESSION['artist'];
 
                         <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Edit Bio:</button>
                         <div id="demo" class="collapse">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+
+                                        <h3><?php echo $ufname ?></h3>
+                                        <h3><?php echo $ulname ?></h3>
+                                        <h3><?php echo $ufname ?></h3>
+                                        <h3><?php echo $email ?></h3>
+
                         </div>
                     </div>
 

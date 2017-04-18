c1375b03af22ab4e50d4bafadfd86171f882a909
ADMIN <morrison0899@gmail.com>
2017-04-18 18:58:55 +0100

    rating commit

--- home.php
+++ home.php
@@ -94,6 +94,7 @@ $artist=$_SESSION['artist'];
 
                </div>
                 <div>
+                    <h1>test</h1>
                     <h2><a href="userprofile.php"><?php $sess  ?></a></h2>
 
                 </div>

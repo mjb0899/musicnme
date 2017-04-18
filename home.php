1145190d6c2227bc19d47cea58c0afeb2bb63176
ADMIN <morrison0899@gmail.com>
2017-04-18 19:48:17 +0100

    rating commit

--- home.php
+++ home.php
@@ -110,6 +110,16 @@ $artist=$_SESSION['artist'];
                                 <h4><?php echo $lastName ?></h4>
                                 <h4><?php echo $userEmail ?></h4>
                                 <button type="button" class="btn-place" data-toggle="modal" ><span class="glyphicon glyphicon-pencil">Edit</button>
+                                <div class="container">
+                                    <h2>Simple Collapsible</h2>
+                                    <p>Click on the button to toggle between showing and hiding content.</p>
+                                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Simple collapsible</button>
+                                    <div id="demo" class="collapse">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </div>
+                                </div>
                             </div>
 
 

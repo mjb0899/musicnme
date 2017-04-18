6051b246da703dd6f90d6eb8325b0953e9b757f8
ADMIN <morrison0899@gmail.com>
2017-04-19 00:42:21 +0100

    rating commit

--- contactUs.php
+++ contactUs.php
@@ -84,7 +84,7 @@ $acc=$_SESSION['acctype'];
                     <label for="matter">Description:</label>
                     <textarea  class="form-control" id="matter" placeholder="Give a short Description" name="matter" required></textarea>
                 </div>
-                <button type="submit" class="btn btn-default" >Submit</button>
+                <input type="submit" class="btn btn-default" >Submit</input>
                 <div id="alert" >Thank you for getting in touch! Our team will get back to you via email!</div>
             </form>
         </div>

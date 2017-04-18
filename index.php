931177b8006daa114f70eb20565597c072889d34
ADMIN <morrison0899@gmail.com>
2017-04-19 00:17:26 +0100

    rating commit

--- index.php
+++ index.php
@@ -39,7 +39,6 @@ session_start();
                     <li <?php if(!isset($sess)) { ?>style="display:none;"<?php } ?>><a href="home.php">Home</a></li>
                     <li <?php if(!isset($sess)) { ?>style="display:none;"<?php } ?>><a href="#">Share Music</a></li>
                     <li <?php if(!isset($sess)) { ?>style="display:none;"<?php } ?>><a href="#">Events</a></li>
-                    <li><a href="#">Get In Touch</a></li>
                     <li><a href="aboutus.php">About Us</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">

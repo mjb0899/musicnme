f532264cfa0bb0ae966c082231f32846e2c7c180
ADMIN <morrison0899@gmail.com>
2017-04-18 20:18:42 +0100

    rating commit

--- home.php
+++ home.php
@@ -110,28 +110,9 @@ $artist=$_SESSION['artist'];
                                 <h4><?php echo $firstName ?></h4>
                                 <h4><?php echo $lastName ?></h4>
                                 <h4><?php echo $userEmail ?></h4>
-                                <button type="button" class="btn-place" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-pencil">Edit</button>
+                          <!--      <button type="button" class="btn-place" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-pencil">Edit</button>-->
                             <!--Modal for update info-->
 
-                                <div class="modal fade" id="myModal" role="dialog">
-                                <div class="modal-dialog modal-sm">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        <h4 class="modal-title">Select File</h4>
-                                    </div>
-                                    <div class="modal-body">
-                                        <form action="uploadImage.php" method="post" >
-                                            <input type="file" name="file" id="exampleInputFile">
-                                            <button type="submit" class="btn btn-default btn-sm" name="submit"> <span class="glyphicon glyphicon-pencil">Upload</span></button>
-                                        </form>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
 
 

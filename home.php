ddd3e0c522632c68304420762f32d8359c7534c3
ADMIN <morrison0899@gmail.com>
2017-04-18 19:10:17 +0100

    rating commit

--- home.php
+++ home.php
@@ -99,7 +99,7 @@ $artist=$_SESSION['artist'];
                 <div class="profile_data">
                     <div class="container">
 
-                        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Edit Bio:</button>
+                        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Edit Profile</button>
                         <div id="demo" class="collapse">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit,
                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,

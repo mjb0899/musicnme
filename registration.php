5c174d6aefe62027cee98166d5902173824c26da
ADMIN <morrison0899@gmail.com>
2017-04-18 01:24:07 +0100

    rating commit

--- registration.php
+++ registration.php
@@ -64,7 +64,7 @@
                     <input type="radio" name="utype" value="artist">Artist
                 </label>
             </div>
-
+            <div class="your-class">
             <label><b>First Name</b></label>
             <input type="text" placeholder="Enter First Name" name="fname" required>
             <label><b>Last Name</b></label>
@@ -78,6 +78,7 @@
             <label><b>Repeat Password</b></label>
             <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
             <input type="checkbox" checked="checked"> Remember me
+            </div>
             <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
             <div class="clearfix">
                 <button type="button" class="cancelbtn">Cancel</button>

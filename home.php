bb298e16ada2c606df8bba7b04d646de5ee584cf
ADMIN <morrison0899@gmail.com>
2017-04-19 18:28:50 +0100

    rating commit

--- home.php
+++ home.php
@@ -156,9 +156,15 @@ $artist=$_SESSION['artist'];
                                     </h4>
                                 </div>
                                 <div id="collapse1" class="panel-collapse collapse in">
-                                    <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                                    <div class="panel-body">
+                                        <?php
+
+
+
+
+
+                                        ?>
+                                    </div>
                                 </div>
                             </div>
                             <div class="panel panel-default">

1870691b169113eac2e3304823681334d71eb3f0
ADMIN <morrison0899@gmail.com>
2017-04-19 21:06:26 +0100

    rating commit

--- userprofile.php
+++ userprofile.php
@@ -297,16 +297,16 @@ if(strcmp($sess,$owner)==0){
 
                     //upload status
                     if($file_title==="status"){
-                        echo "<div class=\"media\">
-                                         <div class=\"media-left\">
-                                             <img src=\"$profile_pic\" class=\"media-object\" style=\"width:60px\">
+                        echo '<div class="media">
+                                         <div class="media-left">
+                                             <img src="'.$profile_pic.'" class="media-object" style="width:60px">
                                          </div>
-                                         <div class=\"media-body\">
-                                          <h4 class=\"media-heading\">".$post_owner."</h4>
-                                                <p>$status</p>
+                                         <div class="media-body">
+                                          <h4 class="media-heading">"'.$post_owner.'"</h4>
+                                                <p>'.$status.'</p>
                                           </div>
                                   </div>
-                                    <hr>";
+                                    <hr>';
                     }
 
 

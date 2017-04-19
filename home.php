b0bd3002ef2f94513dc7ed01a69987a44fd53e63
ADMIN <morrison0899@gmail.com>
2017-04-19 20:10:35 +0100

    rating commit

--- home.php
+++ home.php
@@ -157,21 +157,7 @@ $artist=$_SESSION['artist'];
                                 </div>
                                 <div id="collapse1" class="panel-collapse collapse in">
                                     <div class="panel-body">
-                                        <?php
-
-                                        include("dbConnect.php");
-
-                                        $sql_query = 'SELECT description from statuses where username='.$sess .' && title="status" ORDER BY dateposted DESC LIMIT 1';
-                                        $result = $db -> query($sql_query);
-                                        while($row = $result -> fetch_array()) {
-                                            $description=$row['description'];
-                                            echo'<div>'. $description.'</div>';
-                                        }
-
-
-
-                                        ?>
-
+                                       gge
                                     </div>
                                 </div>
                             </div>

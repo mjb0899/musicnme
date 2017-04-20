48fc75f0e4950e26a7afcec2d6b0e5f21c0009d4
ADMIN <morrison0899@gmail.com>
2017-04-20 04:34:57 +0100

    rating commit

--- savepost.php
+++ savepost.php
@@ -11,31 +11,30 @@ $user=$_SESSION['user'];
 $artist=$_SESSION['artist'];
 $status=$_POST["status"];
 
+    if($status!=null){
+                        include ("dbConnect.php");
+                        //get uid of user
+                        $sql_query = "Select uid from users Where username='$sess'";
+                        $result = $db -> query($sql_query);
+                        while($row = $result -> fetch_array()){
+                                    $userID= $row['uid'];
+                            }
 
-if($status!=null){
-include ("dbConnect.php");
-//get uid of user
-$sql_query = "Select uid from users Where username='$sess'";
-$result = $db -> query($sql_query);
-while($row = $result -> fetch_array()){
-    $userID= $row['uid'];
-}
+                            $date = date('Y-m-d H:i:s');
 
-$date = date('Y-m-d H:i:s');
+                            //reflect changes
+                                $sql="INSERT INTO topic(description,uid,dateposted) VALUES ('$status','$userID','$date')";
 
-//reflect changes
+                                if(mysqli_query($db,$sql)){
+                                }
+                                else{
+                                    echo"Error:".$sql."<br>" . mysqli_error($db);
+                                }
 
+                                echo 1;
 
-$sql="INSERT INTO topic(description,uid,dateposted) VALUES ('$status','$userID','$date')";
-
-if(mysqli_query($db,$sql)){
-
-}
-else{
-    echo"Error:".$sql."<br>" . mysqli_error($db);
-}
-header("location:home.php");
-}else{
-    echo "Empty Post";
-}
+                    }
+                    else{
+                        echo "Empty Post";
+                    }
 

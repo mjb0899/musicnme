3e4e591b533ba46f13e5e84073a1dfff048c7186
ADMIN <morrison0899@gmail.com>
2017-04-20 13:25:14 +0100

    rating commit

--- upload.php
+++ upload.php
@@ -7,37 +7,48 @@
  * Time: 16:13
  */
 session_start();
-$sess=$_SESSION['name'];
-$user=$_SESSION['user'];
-$artist=$_SESSION['artist'];
-$description=$_POST['description_text'];
-$songartist=$_POST['songartist'];
-$songtitle=$_POST['songtitle'];
+$sess        = $_SESSION['name'];
+$user        = $_SESSION['user'];
+$artist      = $_SESSION['artist'];
 
 
-$upload="upload";
-$date = date('Y-m-d H:i:s');
+//Get data
+$description = $_POST['description_text'];
+$songartist  = $_POST['songartist'];
+$songtitle   = $_POST['songtitle'];
+
+
+$upload = "upload";
+$date   = date('Y-m-d H:i:s');
 
 include("dbConnect.php");
 
-if(isset($_POST['submit'])){
-    $file = $_FILES['file'];
-    $fileName=$_FILES['file']['name'];
-    $fileTmpName=$_FILES['file']['tmp_name'];
-    $fileSize=$_FILES['file']['size'];
-    $fileError=$_FILES['file']['error'];
-    $fileType=$_FILES['file']['type'];
-    $fileExt= explode('.',$fileName);
-    $fileActualExt=strtolower(end($fileExt));
-    $allowed_image = array('jpg','jpeg','png');
-    $allowed_media = array('mp4','mp3','txt');
+if (isset($_POST['submit'])) {
+    $file          = $_FILES['file'];
+    $fileName      = $_FILES['file']['name'];
+    $fileTmpName   = $_FILES['file']['tmp_name'];
+    $fileSize      = $_FILES['file']['size'];
+    $fileError     = $_FILES['file']['error'];
+    $fileType      = $_FILES['file']['type'];
+    $fileExt       = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+    $allowed_image = array(
+        'jpg',
+        'jpeg',
+        'png'
+    );
+    $allowed_media = array(
+        'mp4',
+        'mp3',
+        'txt'
+    );
     //upload for images
-    if(in_array($fileActualExt,$allowed_image)){
-        if($fileError===0){
-            if($fileSize<3097152){
-                $fileNameNew = uniqid('',true).".".$fileActualExt;
-                $fileDestination='uploads/images/'.$fileNameNew;
-                move_uploaded_file($fileTmpName,$fileDestination);
+    if (in_array($fileActualExt, $allowed_image)) {
+        if ($fileError === 0) {
+            if ($fileSize < 3097152) {
+                $fileNameNew     = uniqid('', true) . "." . $fileActualExt;
+                $fileDestination = 'uploads/images/' . $fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
 
 
                 //get uid
@@ -50,57 +61,39 @@ if(isset($_POST['submit'])){
                     $userID = $col1;
                 }
 
-                $upload="upload";
+                $upload = "upload";
 
                 $date = date('Y-m-d H:i:s');
 
 
 
                 //preparing
-                $stmt2=$db->prepare("INSERT INTO topic(description,uid,dateposted,title,file_type,path,file_name) VALUES (?,?,?,?,?,?,?)");
-                $stmt2->bind_param('sisssss',$description,$userID,$date,$upload,$fileActualExt,$fileDestination,$fileNameNew);
-                $stmt2-> execute();
-                $stmt2-> store_result();
+                $stmt2 = $db->prepare("INSERT INTO topic(description,uid,dateposted,title,file_type,path,file_name) VALUES (?,?,?,?,?,?,?)");
+                $stmt2->bind_param('sisssss', $description, $userID, $date, $upload, $fileActualExt, $fileDestination, $fileNameNew);
+                $stmt2->execute();
+                $stmt2->store_result();
                 $stmt2->bind_result($col1);
                 header("location:home.php");
 
 
-                /*
-                //insert into topic
-                $sql="INSERT INTO topic(description,uid,dateposted,title,file_type,path,file_name) VALUES ('$description','$userID','$date','$upload','$fileActualExt','$fileDestination','$fileNameNew')";
-                if(mysqli_query($db,$sql)){
-
-                }
-                else{
-                    echo"Error:".$sql."<br>" . mysqli_error($db);
-                }
-
-                header("Location:home.php");
-
-                */
 
 
 
 
 
-
-
-
-
-
-            }else{
+            } else {
                 echo "File too big";
             }
-        }else{
+        } else {
             echo "Something went wrong with your file";
         }
-    }elseif(in_array($fileActualExt,$allowed_media)) {
+    } elseif (in_array($fileActualExt, $allowed_media)) {
         //upload for music
-        if($fileError===0){
-            if($fileSize<12097152){
-                $fileNameNew = uniqid('',true).".".$fileActualExt;
-                $fileDestination='uploads/media/'.$fileNameNew;
-                move_uploaded_file($fileTmpName,$fileDestination);
+        if ($fileError === 0) {
+            if ($fileSize < 12097152) {
+                $fileNameNew     = uniqid('', true) . "." . $fileActualExt;
+                $fileDestination = 'uploads/media/' . $fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
 
                 //insert
 
@@ -120,36 +113,15 @@ if(isset($_POST['submit'])){
                 }
 
 
-      /*          $upload="upload";
 
-                $date = date('Y-m-d H:i:s');*/
 
-                $stmt2=$db->prepare("INSERT INTO topic(description,uid,dateposted,title,file_name,file_type,path) VALUES (?,?,?,?,?,?,?)");
-                $stmt2->bind_param('sisssss',$description,$userID,$date,$upload,$fileNameNew,$fileActualExt,$fileDestination);
-                $stmt2-> execute();
-                $stmt2-> store_result();
+                $stmt2 = $db->prepare("INSERT INTO topic(description,uid,dateposted,title,file_name,file_type,path) VALUES (?,?,?,?,?,?,?)");
+                $stmt2->bind_param('sisssss', $description, $userID, $date, $upload, $fileNameNew, $fileActualExt, $fileDestination);
+                $stmt2->execute();
+                $stmt2->store_result();
                 $stmt2->bind_result($col1);
                 header("location:home.php");
 
-
-
-
-/*
-
-
-
-                //insert into topic
-                $sql="INSERT INTO topic(description,uid,dateposted,title,file_name,file_type,path) VALUES ('$description','$userID','$date','$upload','$fileNameNew','$fileActualExt','$fileDestination')";
-                if(mysqli_query($db,$sql)){
-
-                }
-                else{
-                    echo"Error:".$sql."<br>" . mysqli_error($db);
-                }
-
-
-
-*/
 
 
 
@@ -159,38 +131,25 @@ if(isset($_POST['submit'])){
 
                 //insert into music metadata
 
-                $stmt3=$db->prepare("INSERT INTO music(music_name,artist,file_name) VALUES (?,?,?)");
-                $stmt3->bind_param('sss',$songtitle,$songartist,$fileNameNew);
-                $stmt3-> execute();
-                $stmt3-> store_result();
+                $stmt3 = $db->prepare("INSERT INTO music(music_name,artist,file_name) VALUES (?,?,?)");
+                $stmt3->bind_param('sss', $songtitle, $songartist, $fileNameNew);
+                $stmt3->execute();
+                $stmt3->store_result();
                 $stmt3->bind_result($col1);
                 header("location:home.php");
 
 
 
 
-                //raw sql
-               /*
-                $sql2="INSERT INTO music(music_name,artist,file_name) VALUES ('$songtitle','$songartist','$fileNameNew')";
-                if(mysqli_query($db,$sql2)){
 
-                }
-                else{k
-                    echo"Error:".$sql."<br>" . mysqli_error($db);
-                }
-
-                header("Location:home.php");
-               */
-
-            }else{
+            } else {
                 echo "File too big";
             }
-        }else{
+        } else {
             echo "Something went wrong with your file";
         }
 
-    }
-    else{
+    } else {
 
         echo "You cannot upload files of this type";
     }

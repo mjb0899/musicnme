80a6af44f03ecaff39ad474c3c7190ee8403d53f
ADMIN <morrison0899@gmail.com>
2017-04-20 04:39:40 +0100

    rating commit

--- home.php
+++ home.php
@@ -26,8 +26,6 @@ $artist=$_SESSION['artist'];
     <link rel="stylesheet" type="text/css" href="css/footer.css">
     <link rel="stylesheet" type="text/css" href="css/homepage.css">
     <!-- Rating script and files -->
-    <script src="js/index.js">
-
 
     <link rel="stylesheet" type="text/css" href="css/rating.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">

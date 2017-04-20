98bb22d2e88cafc0013cff8726210f5b69a2ea08
ADMIN <morrison0899@gmail.com>
2017-04-20 12:15:32 +0100

    rating commit

--- saveFeedback.php
+++ saveFeedback.php
@@ -11,13 +11,32 @@ $desc=$_POST['matter'];
 $subject=$_POST['subject'];
 include ("dbConnect.php");
 
-$sql="Select uid from users where username='$sess'";
-$result = $db -> query($sql);
-while($row = $result -> fetch_array()){
-    $uid= $row['uid'];
+try{
+    $stmt1=$db->prepare("Select uid from users where username=?");
+    $stmt1->bind_param('s',$sess);
+    $stmt1-> execute();
+    $stmt1-> store_result();
+    $stmt1->bind_result($col1);
+    while ($stmt1->fetch()) {
+        $uid = $col1;
+    }
+
+
+    $stmt2=$db->prepare("INSERT INTO user_issue(subject,description,uid) VALUES (?,?,?)");
+    $stmt2->bind_param('ssi',$subject,$desc,$uid);
+    $stmt2-> execute();
+    $stmt2-> store_result();
+    $stmt2->bind_result($col1);
+    header("location:contactUs.php");
+}
+catch(PDOException $e)
+{
+    echo "Error: " . $e->getMessage();
 }
 
 
+
+/*
 $sql="INSERT INTO user_issue (subject,description,uid) VALUES ('$subject','$desc','$uid')";
 
 if(mysqli_query($db,$sql)){
@@ -27,3 +46,5 @@ else{
     echo"Error:".$sql."<br>" . mysqli_error($db);
 }
 header("location:contactUs.php");
+
+*/

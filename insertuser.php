27c56952a3bace104921a31fe06b48e268ccb2d7
ADMIN <morrison0899@gmail.com>
2017-04-20 15:54:31 +0100

    rating commit

--- insertuser.php
+++ insertuser.php
@@ -11,12 +11,23 @@ $username=$_POST["username"];
 $fname=$_POST["fname"];
 $lname=$_POST["lname"];
 $email=$_POST["email"];
-
 $type=$_POST['utype'];
-
 $pass = md5($_POST["pass"]);
+$allowed_utype = array('user','artist');
+if(in_array($type,$allowed_utype)) {
+}else{
+    $type='user';
+}
 
 
+$stmt2 = $db->prepare("INSERT INTO users(username,upassword,uemail,ufname,ulname,utype) VALUES (?,?,?,?,?,?)");
+$stmt2->bind_param('ssssss', $username,$pass,$email,$fname,$lname,$type);
+$stmt2->execute();
+$stmt2->store_result();
+$stmt2->bind_result($col1);
+header("location:home.php");
+
+/*
 $sql="INSERT INTO users(username,upassword,uemail,ufname,ulname,utype) VALUES ('$username','$pass','$email','$fname','$lname','$type')";
 
 if(mysqli_query($db,$sql)){
@@ -27,3 +38,4 @@ else{
 }
 header("location:index.php");
 ?>
+*/

aba1ea87353fa5208c4fcc311685d1327ee8251c
ADMIN <morrison0899@gmail.com>
2017-04-21 18:29:49 +0100

    rating commit

--- savepost.php
+++ savepost.php
@@ -7,7 +7,15 @@
  */
 session_start();
 $sess=$_SESSION['name'];
+
+if($sess==null){
+    header("location:pageNotFound.html");
+}
+
+
+
 $user=$_SESSION['user'];
+
 $artist=$_SESSION['artist'];
 $status=$_POST["status"];
 
@@ -39,7 +47,9 @@ try{
 catch(PDOException $e)
 {
     if($status==null){
+
         echo "status Empty";
+
     }else{
         echo "Error: " . $e->getMessage();
 
@@ -50,56 +60,3 @@ catch(PDOException $e)
 
 
 
-
-
-
-
-
-/*
-if($status!=null){
-                        include ("dbConnect.php");
-
-
-                        //get uid of user
-                        $sql_query = "Select uid from users Where username='$sess'";
-                        $result = $db -> query($sql_query);
-                        while($row = $result -> fetch_array()){
-                                    $userID= $row['uid'];
-                            }
-
-            //prepared
-                    $stmt1=$db->prepare("INSERT INTO topic(description,uid,dateposted) VALUES (?,?,?)");
-                    $stmt1->bind_param(sis,'$status','$userID','$date');
-                    $stmt1-> execute();
-                    $stmt1-> store_result();
-                    $stmt1->bind_result($col1);
-
-
-
-
-
-             //date
-                $date = date('Y-m-d H:i:s');
-
-                            //reflect changes
-                                $sql="INSERT INTO topic(description,uid,dateposted) VALUES ('$status','$userID','$date')";
-
-                                if(mysqli_query($db,$sql)){
-                                }
-                                else{
-                                    echo"Error:".$sql."<br>" . mysqli_error($db);
-                                }
-                                header("location:home.php");
-
-
-
-
-
-
-
-    }
-                    else{
-                        echo "Empty Post";
-                    }
-
-*/

85e7909d5b249b0d7a4ce46ee0a2b3e3dfc6b35d
ADMIN <morrison0899@gmail.com>
2017-04-21 13:19:51 +0100

    rating commit

--- ad_site/ad_home.php
+++ ad_site/ad_home.php
@@ -240,18 +240,11 @@ if($dev!=1){
                                         <i class="fa fa-comments fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class="huge">26</div>
-                                        <div>New Comments!</div>
+                                        <div class="huge">Get Value</div>
+                                        <div>POSTS</div>
                                     </div>
                                 </div>
                             </div>
-                            <a href="#">
-                                <div class="panel-footer">
-                                    <span class="pull-left">View Details</span>
-                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6">
